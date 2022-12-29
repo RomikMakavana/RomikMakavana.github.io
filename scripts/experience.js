e20c92670b6dcdf077ccfefe04278a976e0c6153
Prince <princemakavana61@gmail.com>
2022-12-29 14:43:02 +0530

    change

---
+++ scripts/experience.js
@@ -0,0 +1,8 @@
+
+//     experienceListBody = `<div class="experience-block col-auto me-5 w-100">
+//     <h4>${experienceData.role}, <span>${experienceData.role}</span> <span class="experience-timeline">${experienceData.timeline.start} -
+//     ${experienceData.timeline.end}</span></h4>
+//         <hr>
+//     </div>    
+// `
+
