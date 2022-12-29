561f568f83335ce96b2c0d233b49d4e5c209ac92
Romik Makavana <40670677+RomikMakavana@users.noreply.github.com>
2022-12-29 16:50:00 +0530

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -17,59 +17,6 @@ function closeMenu() {
 
 // fullpage customization
 // var fullpageSlide = $('#fullpage').;
-var fullpageSlide;
-const loadFullpageJs = () => {
-  fullpageSlide = new fullpage("#fullpage", {
-    sectionsColor: ['#caaa8d', '#B8B89F', '#caaa8d', '#B8B89F', '#caaa8d', '#B8B89F'],
-    sectionSelector: '.vertical-scrolling',
-    slideSelector: '.horizontal-scrolling',
-    navigation: true,
-    slidesNavigation: true,
-    controlArrows: false,
-    anchors: ['romikMakavana', 'aboutSection', 'experienceSection', 'projectsSection', 'blogsSection', 'contactUsSection'],
-    menu: '#menu',
-
-    afterLoad: function (anchorLink, index) {
-      $header_top.css('background', 'rgba(0, 47, 77, .3)');
-      $nav.css('background', 'rgba(0, 47, 77, .25)');
-      if (index == 5) {
-        $('#fp-nav').hide();
-      }
-    },
-
-    onLeave: function (index, nextIndex, direction) {
-      if (index == 5) {
-        $('#fp-nav').show();
-      }
-    },
-
-    afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-      if (anchorLink == 'fifthSection' && slideIndex == 1) {
-        $.fn.fullpage.setAllowScrolling(false, 'up');
-        $header_top.css('background', 'transparent');
-        $nav.css('background', 'transparent');
-        $(this).css('background', '#374140');
-        $(this).find('h2').css('color', 'white');
-        $(this).find('h3').css('color', 'white');
-        $(this).find('p').css(
-          {
-            'color': '#DC3522',
-            'opacity': 1,
-            'transform': 'translateY(0)'
-          }
-        );
-      }
-    },
-
-    onSlideLeave: function (anchorLink, index, slideIndex, direction) {
-      if (anchorLink == 'fifthSection' && slideIndex == 1) {
-        $.fn.fullpage.setAllowScrolling(true, 'up');
-        $header_top.css('background', 'rgba(0, 47, 77, .3)');
-        $nav.css('background', 'rgba(0, 47, 77, .25)');
-      }
-    }
-  });
-}
 
 
 function setHttpData(apiUrl, method, data) {
@@ -115,7 +62,6 @@ $(document).ready(() => {
                   </div>`;
         })
         $("section.blog-container div.blog-list").html(content.join(" "));
-        loadFullpageJs("#fullpage")
       }
     }
   })
