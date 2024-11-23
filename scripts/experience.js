f59bb65793f01c20399c196b1f63f0ca6e53a4d1
Romik <makavanaromik1214@gmail.com>
2024-11-23 11:51:19 +0530

    v2 ui

--- scripts/experience.js
+++
@@ -1,8 +0,0 @@
-
-//     experienceListBody = `<div class="experience-block col-auto me-5 w-100">
-//     <h4>${experienceData.role}, <span>${experienceData.role}</span> <span class="experience-timeline">${experienceData.timeline.start} -
-//     ${experienceData.timeline.end}</span></h4>
-//         <hr>
-//     </div>    
-// `
-
